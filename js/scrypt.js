a2b853b52e477bc82a0feae9d75627a2cd0bbab7
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-03-10 10:44:58 +0300

    add map

--- js/scrypt.js
+++ js/scrypt.js
@@ -88,6 +88,34 @@ import {calcPrice} from './show-price.js';
 calcPrice.registerEvent()
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------
+// ======================== КАРТА
+// ymaps.ready(init); 
+// var myMap;
+
+// function init() {
+
+//     myMap = new ymaps.Map("map", {
+//         center: [43.238253, 76.945465], // Координаты центра карты
+//         zoom: 13 // Маштаб карты
+//     }); 
+
+//     myMap.controls.add(
+//         new ymaps.control.ZoomControl()  // Добавление элемента управления картой
+//     ); 
+
+//     myPlacemark = new ymaps.Placemark([43.238253,76.945465], { // Координаты метки объекта
+//         balloonContent: "<div class='ya_map'>Заезжайте в гости!</div>" // Подсказка метки
+//     }, {
+//         preset: "twirl#redDotIcon" // Тип метки
+//     });
+    
+//     myMap.geoObjects.add(myPlacemark); // Добавление метки
+//     myPlacemark.balloon.open(); // Открытие подсказки метки
+    
+// };
+
+
+
 
 
 
